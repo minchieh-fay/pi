3cc0934c4e77aa9e35c173603522f3d3782bb689
minchieh <minchieh@hotmail.com>
2026-09-04 17:04:31 +0800

    123123

--- packages/coding-agent/src/cli.ts
+++ packages/coding-agent/src/cli.ts
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
-import { APP_NAME } from "./config.ts";
-import { consumeInternalProcessRole, INTERNAL_PROCESS_ENV } from "./experimental/process.ts";
-import { runServerProcess } from "./experimental/server.ts";
-import { runSessionWorkerProcess } from "./experimental/session-worker.ts";
+
 import { main } from "./main.ts";
 
-//const internalProcessRole = consumeInternalProcessRole();
-//delete process.env[INTERNAL_PROCESS_ENV];"__PI_INTERNAL_SPAWN"
-// process.title = APP_NAME;
-// process.env.PI_CODING_AGENT = "true";
-// process.env.AI_AGENT = "pi";
-// process.emitWarning = (() => {}) as typeof process.emitWarning;
+let aaa = process.argv.slice(2)
 
-// Configure undici's global dispatcher before provider SDKs issue requests.
-// Runtime settings are applied once SettingsManager has loaded global/project settings.
-
-main(process.argv.slice(2));
+main(aaa);
 
